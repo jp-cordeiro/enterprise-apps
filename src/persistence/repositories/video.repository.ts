52a7447268117dacbac55e95db72a1ede261af76
jp-cordeiro <jpc.araujo@hotmail.com>
2025-05-20 14:30:21 -0300

    feat: use repositories with typeorm

--- src/persistence/repositories/video.repository.ts
+++ src/persistence/repositories/video.repository.ts
@@ -1,43 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
-import { VideoEntity } from '@src/core/entities/video.entity';
-import { PrismaService } from '@src/persistence/prisma/prisma.service';
+import { Inject, Injectable } from '@nestjs/common';
+import { DefaultTypeOrmRepository } from '@src/infra/module/typeorm/repository/default-typeorm.repository';
+import { Video } from '../entities/video.entity';
+import { DataSource } from 'typeorm';
 
 @Injectable()
-export class VideoRepository {
-  private readonly model: PrismaService['video'];
-
-  constructor(prismaService: PrismaService) {
-    this.model = prismaService.video;
-  }
-
-  async findById(id: string): Promise<VideoEntity | null> {
-    try {
-      const videoData = await this.model.findUnique({
-        where: { id },
-      });
-      if (!videoData) {
-        return null;
-      }
-
-      return VideoEntity.createFrom(videoData);
-    } catch (error) {
-      this.handleAndThrowError(error);
-    }
-  }
-  private extractErrorMessage(error: unknown): string {
-    if (error instanceof Error && error.message) {
-      return error.message;
-    }
-    return 'An unexpected error occurred.';
-  }
-
-  protected handleAndThrowError(error: unknown): never {
-    const errorMessage = this.extractErrorMessage(error);
-    if (error instanceof Prisma.PrismaClientValidationError) {
-      throw new Error(error.message);
-    }
-
-    throw new Error(errorMessage);
+export class VideoRepository extends DefaultTypeOrmRepository<Video> {
+  constructor(@Inject(DataSource) readonly datasource: DataSource) {
+    super(Video, datasource);
   }
 }
