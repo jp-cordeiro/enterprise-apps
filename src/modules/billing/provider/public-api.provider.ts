ad0870b3dbcbc756c9e3a72b2e2d5ae100e7cb49
jp-cordeiro <jpc.araujo@hotmail.com>
2025-06-09 14:27:26 -0300

    fix: add subscription status on if clause

--- src/modules/billing/provider/public-api.provider.ts
+++ src/modules/billing/provider/public-api.provider.ts
@@ -1,3 +1,4 @@
+import { SubscriptionStatus } from '@billingModule/core/models/subscription.model';
 import { SubscriptionService } from '@billingModule/core/services/subscription.service';
 import { Injectable } from '@nestjs/common';
 import { BillingSubscriptionStatusApi } from '@sharedModules/integration/interfaces/billing-integration.interface';
@@ -8,7 +9,7 @@ export class BillingPublicApiProvider implements BillingSubscriptionStatusApi {
   async isUserSubscriptionActive(userId: string): Promise<boolean> {
     const subscription =
       await this.subscriptionService.getSubscriptionByUserId(userId);
-    if (subscription?.status) {
+    if (subscription?.status === SubscriptionStatus.Active) {
       return true;
     }
     return false;
